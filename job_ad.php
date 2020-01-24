f204ec92e6a44bfec9df7be7942f753aee3f9065
NorthWestGameStudio <54814804+NorthWestGameStudio@users.noreply.github.com>
2020-01-24 22:53:03 +0100

    Cleaned job_ad.php

--- job_ad.php
+++ job_ad.php
@@ -1,52 +1,57 @@
-<?php $host = 'localhost:3306';    
+<?php 
+//GET request parameters
 $requested_ad_id = $_GET['id'];
-		$conn = mysqli_connect($host, "system_user_vtc", "8rh98w23nrfubsediofnm<pbi9ufuoipbgiwtFFF","vtcmanager"); 
-		if(! $conn )  
-		{  
-			die("2");  
-		}  
+
+//connect to DB
+$host = 'localhost:3306';
+$conn = mysqli_connect($host, "system_user_vtc", "8rh98w23nrfubsediofnm<pbi9ufuoipbgiwtFFF","vtcmanager"); 
+if(! $conn )  
+{
+	die("Can't connect to database");  
+}  
+
+//hole Informationen der Stellenanzeige
 $sql = "SELECT * FROM job_market WHERE AdID=$requested_ad_id";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while($row = $result->fetch_assoc()) {
-				$byCompanyID = $row["byCompanyID"];
-				$AdID = $row["AdID"];
-				$rank = $row["rank"];
-				$sql2 = "SELECT * FROM company_information_table WHERE id=$byCompanyID";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$byCompanyID = $row["byCompanyID"];
+		$AdID = $row["AdID"];
+		$rank = $row["rank"];
+		
+		//hole Firmenname der Stellenanzeige
+		$sql2 = "SELECT * FROM company_information_table WHERE id=$byCompanyID";
 		$result2 = $conn->query($sql2);
 		if ($result2->num_rows > 0) {
-			// output data of each row
 			while($row = $result2->fetch_assoc()) {
 				$byCompanyname = $row["name"];
 			}
-		}
-		?>
+		}?>
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-	  <title><?php echo $byCompanyname; ?> - VTCManager</title>
-	  <?php include 'basis_header.php'; ?>  
-  </head>
-  <body>
-	  <?php include 'navbar.php'; ?>  
-	      <footer class="footer">
-        <div class="container">
-			<?php 
-				$job_desc = file_get_contents("https://vtc.northwestvideo.de/media/articles/ad_description/".$AdID.'.txt');
-				
-				echo <<<EOT
-				<h2>$byCompanyname - $rank gesucht!</h2>
-				<p>$job_desc</p>
-				EOT;
-			}
-		} else {
-		}?>
-			<button type="button" class="btn btn-success" onclick="location.href = 'https://vtc.northwestvideo.de/company/apply?ad_id=<?php echo $AdID;?>';">Bewerben</button>
-			<hr>
-			<br>
-			<p>Seite neu laden um weitere Ergebnisse zu sehen.</p>
-			<br>
+	<head>
+		<title><?php echo $byCompanyname; ?> - VTCManager</title>
+		<?php include 'basis_header.php'; ?>  
+	</head>
+	<body>
+		<?php include 'navbar.php'; ?>  
+		<footer class="footer">
+			<div class="container">
+				<?php 
+		//lade Beschreibung der Stellenanzeige
+		$job_desc = file_get_contents("https://vtc.northwestvideo.de/media/articles/ad_description/".$AdID.'.txt');
+		//Ausgabe der gesamten Stellenanzeige
+		echo <<<EOT
+		<h2>$byCompanyname - $rank gesucht!</h2>
+		<p>$job_desc</p>
+		EOT;
+	}
+}?>
+				<button type="button" class="btn btn-success" onclick="location.href = 'https://vtc.northwestvideo.de/company/apply?ad_id=<?php echo $AdID;?>';">Bewerben</button>
+				<hr>
+				<br>
+				<p>Seite neu laden um weitere Ergebnisse zu sehen.</p>
+				<br>
             <div class="col-md-9 social-media">
                 <p class="pull-left">
                     <a href="https://vtc.northwestvideo.de/impressum">Impressum</a>|
