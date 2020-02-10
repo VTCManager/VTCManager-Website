e8fca48ffcfdf4f8a152d409f04858e23178f1a8
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-10 15:34:28 +0100

    added view application

--- company/applications/load_applications.php
+++ company/applications/load_applications.php
@@ -1,15 +1,36 @@
 <?php
-$sql = "SELECT * FROM application WHERE forCompanyID=$requested_appli_id";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
+$sql = "SELECT * FROM application WHERE forCompanyID=$company ORDER BY time DESC";
+		$result1 = $conn->query($sql);
+		if ($result1->num_rows > 0) {
 			// output data of each row
-			while($row = $result->fetch_assoc()) {
+			while($row = $result1->fetch_assoc()) {
 				$byUserID = $row["byUserID"];
-				$forCompanyID = $row["forCompanyID"];
 				$forRank = $row["forRank"];
 				$appli_status = $row["status"];
+				$appli_time = $row["time"];
+				$appli_id = $row["applicationID"];
+				$sql = "SELECT * FROM user_data WHERE userID=$byUserID";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						$appli_username = $row["username"];
+						}
+				} else {
+					$appli_username = "Unbekannt";
+				}
+				?>
+				<tr>
+				<td><a href="https://vtc.northwestvideo.de/account/?userid=<?php echo $byUserID;?>"><?php echo $appli_username;?></a></td>
+                <td><?php echo $forRank;?></td>
+                <td><?php echo $appli_status;?></td>
+                <td><?php echo $appli_time;?></td>
+				<td><form action="view" method="post" name="createnewrankForm" id="createnewrankForm">
+					<button class="btn btn-info" name="id" value="<?php echo $appli_id;?>">Öffnen</button>
+			</form></td>
+		</tr>
+				<?php
 					}
 		} else {
-			echo("Keine Bewebungen gefunden!")
+			echo("Keine Bewebungen gefunden!");
 		}
 		?>
--- company/applications/view.php
+++ company/applications/view.php
@@ -1,4 +1,9 @@
-<?php $host = 'localhost:3306';    
+<?php 
+if(!isset($_POST['id'])) {
+	die("Keine Bewerbungsdaten abrufbar. Öffne die Bewerbungsliste erneut");
+}
+$application_id = $_POST['id'];
+$host = 'localhost:3306';    
 		$conn = mysqli_connect($host, "system_user_vtc", "8rh98w23nrfubsediofnm<pbi9ufuoipbgiwtFFF","vtcmanager"); 
 		if(! $conn )  
 		{  
@@ -52,6 +57,16 @@ $sql = "SELECT * FROM rank WHERE name='$rank_user' AND forCompanyID=$company";
 		} else {
 		die("no res 1");
 		}
+		if($EditEmployees != "1"){
+	die("Berechtigung fehlt.");
+}
+if(isset($_POST['cmd'])) {
+	if($_POST['cmd'] == "accept"){
+		
+	}else if($_POST['cmd'] == "decline") {
+			
+	}
+}
 		$sql = "SELECT * FROM company_information_table WHERE id=$company";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
@@ -62,10 +77,35 @@ $sql = "SELECT * FROM rank WHERE name='$rank_user' AND forCompanyID=$company";
 		} else {
 		die("no res 1");
 		}
-if($EditEmployees != "1"){
-	die("Berechtigung fehlt.");
-}
-
+		$sql = "SELECT * FROM user_data WHERE userID=$byUserID";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						$appli_username = $row["username"];
+						}
+				} else {
+					$appli_username = "Unbekannt";
+				}
+$sql = "SELECT * FROM application WHERE applicationID=$application_id";
+		$result1 = $conn->query($sql);
+		if ($result1->num_rows > 0) {
+			// output data of each row
+			while($row = $result1->fetch_assoc()) {
+				$byUserID = $row["byUserID"];
+				$forRank = $row["forRank"];
+				$appli_status = $row["status"];
+				$appli_time = $row["time"];
+			}
+		}
+		$sql = "SELECT * FROM user_data WHERE userID=$byUserID";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						$appli_username = $row["username"];
+						}
+				} else {
+					$appli_username = "Unbekannt";
+				}
 		?>
 <!DOCTYPE html>
 <html lang="de">
@@ -75,24 +115,13 @@ if($EditEmployees != "1"){
   </head>
   <body>
 	  <?php include '../../navbar.php'; ?>  
-        <div class="container">
-			<h1>Bewerbungen von <?php echo $company_name;?></h1>
-			<div class="vertical-scroll">
-            <table class="table">
-                <thead>
-                    <tr>
-						<td>Bewerber</td>
-                        <td>Rolle</td>
-                        <td>Status</td>
-                        <td>Datum</td>
-						<td></td>
-                    </tr>
-                </thead>
-				<tbody>
-					<?php include 'load_applications.php'; ?>                  
-                </tbody>
-            </table>
-            </div>
+	  <div class="container">
+			<h1>Bewerbungen von <?php echo $appli_username;?></h1>
+			<p>Rolle: <?php echo $forRank;?><br>
+			   Abgesendet um <?php echo $appli_time;?><br>
+			   Status: <?php echo $appli_status;?></p>
+			   <form action="" method="post" name="createnewrankForm" id="createnewrankForm"><input type="hidden" value="accept" name="cmd" /><button class="btn btn-success" name="id" value="<?php echo $application_id;?>">Akzeptieren</button></form>
+			   <form action="" method="post" name="createnewrankForm" id="createnewrankForm"><input type="hidden" value="decline" name="cmd" /><button class="btn btn-danger" name="id" value="<?php echo $application_id;?>">Ablehnen</button></form>
             <footer class="footer">
             <div class="col-md-9 social-media">
                 <p class="pull-left">
